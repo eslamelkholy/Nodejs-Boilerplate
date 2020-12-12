f00e5f8962865f4ec409ecb395918ceec8537d2f
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-12-12 20:03:00 +0200

    Added Express Middlewares in Separate File

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import Router from "./routes/index";
 import "./models";
+import expressMiddlewares from "./middlewares/expressMiddlewares";
 
 const app = express();
+const PORT = process.env.PORT;
 
+expressMiddlewares(app);
 app.use("", Router);
 
-app.listen(3000, function () {
-  console.log("running on port 3000 ");
-});
+app.listen(PORT, () => console.log(`Nodejs Running on Port ${PORT}`));
--- src/routes/courses.js
+++ src/routes/courses.js
@@ -2,6 +2,6 @@ import express from "express";
 import courses from "../controller/courses";
 const coursesRouter = express.Router();
 
-coursesRouter.post("/", courses.get);
+coursesRouter.get("/", courses.get);
 
 export default coursesRouter;
