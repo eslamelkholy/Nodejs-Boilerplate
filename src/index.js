4b13c381a14c4252d29ce188e9a8251c0f59c03f
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-12-13 12:37:56 +0200

    Add Loggingn For Monitoring Errors At Central Error Handler

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import Router from "./routes/index";
 import "./models";
 import expressMiddlewares from "./middlewares/expressMiddlewares";
-import handleError from "./Shared/lib/error";
+import { handleError } from "./Shared/lib/error";
 import logger from "./Shared/lib/logger";
 
 const app = express();
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 6666;
 expressMiddlewares(app);
 app.use("", Router);
 
-app.use((err, req, res, next) => handleError(err, res));
+app.use((err, req, res, next) => {
+  logger.error(err);
+  handleError(err, res);
+});
 
 app.listen(PORT, () => logger.info(`Nodejs Running on Port ${PORT}`));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import courses from "./courses";
-import ErrorHandler from "../Shared/lib/error";
+import { ErrorHandler } from "../Shared/lib/error";
 const app = express();
 
 app.get("/error", (req, res) => {
   throw new ErrorHandler(500, "Internal server error");
 });
+
 app.use("/courses", courses);
-app.get('/',(req,res)=>res.json({message:'welcome'}))
+
+app.get("/", (req, res) => res.json({ message: "welcome" }));
 
 export default app;
