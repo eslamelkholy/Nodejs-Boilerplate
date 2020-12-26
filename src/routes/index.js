6a2df0058866e8c6d47300db8363db506f539e69
Eslam Elkholy <eslamelkholy5325762@gmail.com>
2020-12-26 16:31:12 +0200

    Adde Nginx Within Docker Compose Services To Acts as Reverse Proxy and Doing Some Load Balancing

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,6 +16,8 @@ app.use("/courses", courses);
 app.get("/", (req, res) =>
   res.json({
     message1: "Changes in Code in Development Now Reflects in Docker",
+    message2: "Added Nginx Configuration File And Config Directory For Certifications",
+    message3: "Added Nginx Within Docker Compose.yml To Acts As Reverse Proxy and Doing Some Load Balancing",
   })
 );
 
